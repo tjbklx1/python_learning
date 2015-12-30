160f627d49a2a788791fa91c290c85786a1426e8
tjbklx1 <wolfs_2004@aliyun.com>
2015-12-30 08:19:46 +0800

    update login

---
+++ flask/microblog01/app05_login/app05/views.py
@@ -0,0 +1,44 @@
+from flask import render_template, flash, redirect, session, url_for, request, g
+from app05 import app, db, lm, oid
+from forms import LoginForm
+from models import User
+
+
+@lm.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = { 'nickname': 'Miguel' }
+    posts = [
+        { 
+            'author': { 'nickname': 'John' }, 
+            'body': 'Beautiful day in Portland!' 
+        },
+        { 
+            'author': { 'nickname': 'Susan' }, 
+            'body': 'The Avengers movie was so cool!' 
+        }
+    ]
+    return render_template('index.html',
+        title = 'Home',
+        user = user,
+        posts = posts)
+
+@app.route('/login', methods = ['GET', 'POST'])
+@oid.loginhandler
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for OpenID="' + form.openid.data + '", remember_me=' + str(form.remember_me.data))
+        return redirect('/index')
+    return render_template('login.html', 
+        title = 'Sign In',
+        form = form,
+        providers = app.config['OPENID_PROVIDERS'])
+    
+
+
+
